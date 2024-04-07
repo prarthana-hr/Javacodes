8b3587274fb6622df51193565939b28c27f4e18e
Prarthana <64904371+prarthana-hr@users.noreply.github.com>
2024-04-07 11:56:03 +0530

    Add files via upload

---
+++ World.java
@@ -0,0 +1,14 @@
+package com.sample.main;
+
+public class World {
+
+	public static void main(String[] args) {
+		
+		for(int a=1;a<=10;a++) {
+		System.out.println("New program!!" +a);
+		}
+	
+
+	}
+
+}
